466c8ece28d047492af5720a90c3405d9b7d66a9
Ned Ruggeri <ruggeri@self-loop.com>
2017-03-25 15:18:14 -0700

    Initial Commit

---
+++ Practice2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practice2
+//
+//  Created by Ned Ruggeri on 3/25/17.
+//  Copyright © 2017 Ned Ruggeri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
